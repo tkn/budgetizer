e0720078fff0a3c335bc98be6db81920bdf51696
Torben Nielsen <github@torbennielsen.org>
2024-11-01 10:50:22 +0000

    First commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+from st_supabase_connection import SupabaseConnection
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = conn.query("*", table="budgets", ttl="10m").execute()
+
+# Print results.
+for row in rows.data:
+    st.write(f"{row['name']} has a :{row['year']}:")

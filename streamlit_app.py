99a1896eea6979147471271062f244fa6fc0d6c1
Torben Nielsen <github@torbennielsen.org>
2024-11-01 11:11:56 +0000

    supabase conn working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
-from st_supabase_connection import SupabaseConnection
+from st_supabase_connection import SupabaseConnection, execute_query
 
 # Initialize connection.
 conn = st.connection("supabase",type=SupabaseConnection)
 
 # Perform query.
-rows = conn.query("*", table="budgets", ttl="10m").execute()
+rows = execute_query(conn.table("budgets").select("*"))
+
+st.write(rows)
 
 # Print results.
 for row in rows.data:
